44e7b7344496793b4e0855a3fb615dde939eee3f
VallejoAgustin <AgustinVallejo@my.unt.edu>
2018-08-19 15:24:54 -0500

    added screen and background

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,5 +1,6 @@
 // Timber.cpp : Defines the entry point for the console application.
 #include "stdafx.h"
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
@@ -10,6 +11,38 @@ int main(){
 
 	//create and open window
 	RenderWindow window(vm, "Timber!!", Style::Fullscreen);
+
+	//create texture object and load image from file
+	Texture textureBackground;
+	if (!textureBackground.loadFromFile("graphics/background.png")) {
+		std::cout << "ERROR: image could not be loaded!";
+		return 1;
+		//TODO: try/catch exception
+	}
+
+	//set a sprite object and position
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+
+	while (window.isOpen()) {
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+			window.close();
+		}
+
+		//update scene
+
+		//draw scene
+
+		//clear from last frame
+		window.clear();
+
+		//draw scene background
+		window.draw(spriteBackground);
+
+		//display drawn for current frame
+		window.display();
+	}
 
     return 0;
 }
