451960167ee33f9720ad715c49919abc400f645a
VallejoAgustin <AgustinVallejo@my.unt.edu>
2018-08-20 12:32:30 -0500

    modified cloud code for modification/readibility

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -20,8 +20,6 @@ int main(){
 		return 1;
 		//TODO: try/catch exception
 	}
-
-	//set a sprite object and position
 	Sprite spriteBackground;
 	spriteBackground.setTexture(textureBackground);
 	spriteBackground.setPosition(0, 0);
@@ -46,16 +44,17 @@ int main(){
 	//load cloud
 	Texture textureCloud;
 	textureCloud.loadFromFile("graphics/cloud.png");
-	Sprite spriteCloud1, spriteCloud2, spriteCloud3;
-	spriteCloud1.setTexture(textureCloud);
-	spriteCloud2.setTexture(textureCloud);
-	spriteCloud3.setTexture(textureCloud);
-	//position clouds 'off-screen'
-	spriteCloud1.setPosition(0, 0);
-	spriteCloud2.setPosition(0, 250);
-	spriteCloud3.setPosition(0, 500);
-	bool cloud1Active = false, cloud2Active = false, cloud3Active = false;
-	float cloud1Speed = 0.0, cloud2Speed = 0.0, cloud3Speed = 0.0;//not using 0.0f
+	int numberOfClouds = 5;
+	Sprite *spriteCloud = new Sprite[numberOfClouds];
+	bool *cloudActive = new bool[numberOfClouds];
+	float *cloudSpeed = new float [numberOfClouds];
+
+	for (int i = 0; i < numberOfClouds; i++) {
+		spriteCloud[i].setTexture(textureCloud);
+		spriteCloud[i].setPosition(0, 250 * i);
+		cloudActive[i] = false;
+		cloudSpeed[i] = 0.0;
+	}
 
 	//to control framerate
 	Clock clock;
@@ -105,7 +104,7 @@ int main(){
 			window.close();
 		}
 
-		//start game
+		//start/restart game
 		if (Keyboard::isKeyPressed(Keyboard::Return)) {
 			paused = false;
 
@@ -157,57 +156,24 @@ int main(){
 				}
 			}
 
+			//move clouds
+			for (int i = 0; i < numberOfClouds; i++) {
+				if (!cloudActive[i]) {
+					cloudSpeed[i] = rand() % ((i + 2) * 50) + 30;//to achieve p-random (different) speed
+					
+					float height = rand() % 150;//p-random diff height
+					spriteCloud[i].setPosition(-300, height);
+					cloudActive[i] = true;
+				}
+				else {
+					spriteCloud[i].setPosition(spriteCloud[i].getPosition().x + (cloudSpeed[i] * dt.asSeconds()), spriteCloud[i].getPosition().y);
+
+					if (spriteCloud[i].getPosition().x > 1920) {
+						cloudActive[i] = false;
+					}
+				}
+			}
 			//TODO: create function that takes in object, modifier (for rand), and modifies accordingly
-			if (!cloud1Active) {
-				srand((int)time(0) * 10);
-				cloud1Speed = rand() % 200;
-
-				srand((int)time(0) * 10);
-				float height = rand() % 150;
-				spriteCloud1.setPosition(-300, height);
-				cloud1Active = true;
-			}
-			else {
-				spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()), spriteCloud1.getPosition().y);
-
-				if (spriteCloud1.getPosition().x > 1920) {
-					cloud1Active = false;
-				}
-			}
-
-			if (!cloud2Active) {
-				srand((int)time(0) * 20);
-				cloud2Speed = rand() % 200;
-
-				srand((int)time(0) * 20);
-				float height = rand() % 150;
-				spriteCloud2.setPosition(-300, height);
-				cloud2Active = true;
-			}
-			else {
-				spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()), spriteCloud2.getPosition().y);
-
-				if (spriteCloud2.getPosition().x > 1920) {
-					cloud2Active = false;
-				}
-			}
-
-			if (!cloud3Active) {
-				srand((int)time(0) * 30);
-				cloud3Speed = rand() % 200;
-
-				srand((int)time(0) * 30);
-				float height = rand() % 150;
-				spriteCloud3.setPosition(-300, height);
-				cloud3Active = true;
-			}
-			else {
-				spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()), spriteCloud3.getPosition().y);
-
-				if (spriteCloud3.getPosition().x > 1920) {
-					cloud3Active = false;
-				}
-			}
 
 			std::stringstream ss;
 			ss << "Score = " << score;
@@ -222,9 +188,10 @@ int main(){
 		//draw scene background
 		window.draw(spriteBackground);
 		//clouds
-		window.draw(spriteCloud1);
-		window.draw(spriteCloud2);
-		window.draw(spriteCloud3);
+		for (int i = 0; i < numberOfClouds; i++) {
+			window.draw(spriteCloud[i]);
+		}
+		
 		//tree
 		window.draw(spriteTree);
 		//bee
